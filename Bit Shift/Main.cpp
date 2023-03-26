a59d69010df1a363c6a06f366a48f62680ab8952
Repti <Repti@BOX>
2023-03-26 14:10:31 +0500

    Add project files.

---
+++ Bit Shift/Main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace  std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int a, b;
+	cout << "Введите два числа: ";
+	cin >> a >> b;
+	cout << endl;
+			a ^= b;
+			b ^= a;
+			a ^= b;
+			cout << "Вывод: ";
+	cout << a << " " << b;
+}
